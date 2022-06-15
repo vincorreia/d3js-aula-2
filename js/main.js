c5491bbd460c88f46386e8f32b82581e8103a45f
Vinncius Correia <vcorreiaes@gmail.com>
2022-06-15 14:18:12 -0300

    Make the graph dynamic between Profit and Revenue values

--- js/main.js
+++ js/main.js
@@ -4,6 +4,22 @@
 *    Project 1 - Star Break Coffee
 */
 
+const MARGIN = {LEFT: 100, RIGHT: 20, TOP: 20, BOTTOM: 100}
+const WIDTH = 600 - MARGIN.LEFT - MARGIN.RIGHT
+const HEIGHT = 400 - MARGIN.TOP - MARGIN.BOTTOM
+
+let flag = true
+
+const svg = d3.selectAll("#chart-area")
+.append("svg")
+    .attr("width", WIDTH + MARGIN.LEFT + MARGIN.RIGHT)
+    .attr("height", HEIGHT + MARGIN.TOP + MARGIN.BOTTOM)
+
+
+const g = svg.append("g")
+    .attr("transform", `translate(${MARGIN.LEFT}, ${MARGIN.TOP})`)
+
+
 d3.csv("../data/revenues.csv")
     .then(data => {
         data.forEach(d => {
@@ -11,19 +27,13 @@ d3.csv("../data/revenues.csv")
             d.profit = Number(d.profit)
         })
         
-        const MARGIN = {LEFT: 100, RIGHT: 20, TOP: 20, BOTTOM: 100}
-        const WIDTH = 600 - MARGIN.LEFT - MARGIN.RIGHT
-        const HEIGHT = 400 - MARGIN.TOP - MARGIN.BOTTOM
+        d3.interval(() => {
+            flag = !flag
+            update(data)
+        }, 1000)
 
-        const svg = d3.selectAll("#chart-area")
-        .append("svg")
-            .attr("width", WIDTH + MARGIN.LEFT + MARGIN.RIGHT)
-            .attr("height", HEIGHT + MARGIN.TOP + MARGIN.BOTTOM)
-        
-        
-        const g = svg.append("g")
-            .attr("transform", `translate(${MARGIN.LEFT}, ${MARGIN.TOP})`)
 
+        
         const x = d3.scaleBand()
             .domain(data.map(d => d.month))
             .range([0, WIDTH])
@@ -33,11 +43,6 @@ d3.csv("../data/revenues.csv")
         const y = d3.scaleLinear()
             .domain([0, d3.max(data, d => d.revenue)])
             .range([HEIGHT, 0])
-        
-
-        d3.interval(() => {
-            update(data)
-        }, 1000)
 
         const xAxisGroup = g.append("g")
         .attr("class", "x axis")
@@ -45,9 +50,29 @@ d3.csv("../data/revenues.csv")
         const yAxisGroup = g.append("g")
         .attr("class", "y axis")
 
+        // Y Label
+        let yAxisLabel = g.append("text")
+        .attr("class", "y axis-label")
+        .attr("transform", "rotate(-90)")
+        .attr("font-size", "20px")
+        .attr("x", - (HEIGHT / 2 + 60))
+        .attr("y", - 60)
+        .text("Revenue ($)")
+
+        // X Label
+        g.append("text")
+        .attr("class", "x axis-label")
+        .attr("font-size", "20")
+        .text("Month")
+        .attr("x", (WIDTH / 2) - 30)
+        .attr("y", HEIGHT + 60)
+
         function update(data) {
+
+            let value = flag ? "profit" : "revenue"
+
             x.domain(data.map(d => d.month))
-            y.domain([0, d3.max(data, d => d.revenue)])
+            y.domain([0, d3.max(data, d => d[value])])
         
         const xAxisCall = d3.axisBottom(x)
 
@@ -59,30 +84,28 @@ d3.csv("../data/revenues.csv")
 
             yAxisGroup
                 .call(yAxisCall)
-            }
-        // X Label
-        g.append("text")
-            .attr("class", "x axis-label")
-            .attr("font-size", "20")
-            .text("Month")
-            .attr("x", (WIDTH / 2) - 30)
-            .attr("y", HEIGHT + 60)
-
-        // Y Label
-        g.append("text")
-            .attr("class", "y axis-label")
-            .attr("transform", "rotate(-90)")
-            .attr("font-size", "20")
-            .text("Revenue ($)")
-            .attr("x", - (HEIGHT / 2 + 60))
-            .attr("y", - 60)
+            
+        
         const rects = g.selectAll("rect")
             .data(data)
+        
+        rects.exit().remove()
+
+        rects
+            .attr("x", d => x(d.month))
+            .attr("y", d => y(d[value]))
+            .attr("height", d => HEIGHT - y(d[value]))
+            .attr("width", x.bandwidth)
+            .attr("fill", "grey")
 
         rects.enter().append("rect")
             .attr("x", d => x(d.month))
-            .attr("y", d => y(d.revenue))
-            .attr("height", d => HEIGHT - y(d.revenue))
+            .attr("y", d => y(d[value]))
+            .attr("height", d => HEIGHT - y(d[value]))
             .attr("width", x.bandwidth)
             .attr("fill", "grey")
+
+            let text = flag ? "Profit ($)" : "Revenue ($)"
+            yAxisLabel.text(text)
+        }
 })
